1399f270fb99177630a9e7aaee977b9bf4b0545a
prowebm <prowebmaster@yandex.com>
2025-06-30 23:46:32 +0100

    Create renderPortableText.ts

---
+++ src/lib/renderPortableText.ts
@@ -0,0 +1,14 @@
+﻿import { toHTML } from '@portabletext/to-html'
+
+export function renderPortableText(value: any) {
+  return toHTML(value, {
+    components: {
+      types: {
+        image: ({ value }) => {
+          const url = value?.asset?._ref || ''
+          return `<img src="${url}" alt="${value.alt || ''}" />`
+        },
+      },
+    },
+  })
+}
